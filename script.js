102027a412ea31992291802ce89d666e2b9ab6a6
Alireza Shirzadi <shirzadi.alireza.9821160025@example.com>
2023-11-27 16:32:21 +0330

    tamrin 7

---
+++ script.js
@@ -0,0 +1,9 @@
+let apiTitle = document.getElementById("api-title");
+let firstP = document.getElementsByClassName("first-p");
+let pTags = document.getElementsByTagName("p");
+const x = document.querySelectorAll("h2.second-title");
+
+apiTitle.innerHTML = "این متن با جاوااسکریپت تغییر کرده"
+pTags.innerHTML = "این متن با جاوااسکریپت تغییر کرده";
+x.style.backgroundColor = "green"
+firstP.style.color = "Blue"
